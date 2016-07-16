1da56d79ce45ba4647d592013ef3bf1ad9f943f2
Saulo Antunes <sauloantuness@gmail.com>
2016-07-16 11:02:41 -0300

    Add WhiteNoise

--- ticc/settings.py
+++ ticc/settings.py
@@ -126,3 +126,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticroot')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
