9792c4bbc38cf6ce06e492803cf3b7e55412d99c
luisccf <luisccf@me.com>
2016-07-23 17:50:49 -0300

    atualiza home page

--- static/dist/js/script.js
+++ static/dist/js/script.js
@@ -1,22 +1,3 @@
-function initMap() {
-    var map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -20.172307, lng: -44.910105},
-        zoom: 13
-    });
-    var info = new google.maps.InfoWindow({
-        content: '<h5>Centro Federal de Educação Tecnológica de Minas Gerais, <b>Campus V</b> - Divinópolis</h5>',
-        maxWidth: 250
-    });
-    var marker = new google.maps.Marker({
-        position: {lat: -20.172307, lng: -44.910105},
-        map: map,
-        title: 'sede'
-    });
-    marker.addListener('click', function() {
-        info.open(map, marker);
-    });
-}
-
 // $(function(){
 //     $(".box").hide().each(function(i) {
 //         $(this).delay(i*600).fadeIn(600);
