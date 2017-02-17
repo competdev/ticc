a22eadb7087ed451d3b61cde089b9bb7f41bde5d
Bernard Menezes <bernard_menezes@hotmail.com>
2017-02-17 15:13:25 -0200

    Correção de bug da function edit_or_delete

--- static/dist/js/dialog.js
+++ static/dist/js/dialog.js
@@ -1,24 +1,20 @@
 function showDialog(message, link) {
+	$('#edit').hide();
+	$('#del').hide();
 	$('#message').html(message);
 	$('#link').attr('href', link);
 }
 
-function edit_or_delete(edit_href, delete_href){
-
-	var edit = $(document.createElement('a'))
-	.attr({'href':edit_href,'class': 'btn btn-warning pull-left', 'style': 'width: 20%; margin-left: 110px'})
-	.html('Editar');
-
-	var del = $(document.createElement('a'))
-	.attr({'href':delete_href ,'class': 'btn btn-danger pull-left', 'style': 'width: 20%; margin-left: 130px'})
-	.html('Excluir');
-
+function edit_or_delete(edit_href, del_href){
 	$('.modal-title')
 	.html('Escolha uma opção:')
 	.attr({'align': 'center'});
 
+	$('.modal-body').hide();
+
+	$('#edit').attr({'href': edit_href, 'style': 'width:20%; margin-left: 110px'});
+	$('#del').attr({'href': del_href, 'style': 'width:20%; margin-right: 110px'});
+
 	$('#cancel').hide();
-	$('a').hide();
-	$('#link').attr('href', '/resultados');
-	$('.modal-body').append(edit, del);
+	$('#link').hide();
 }
