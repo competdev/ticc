95a27b67ca6a85cc767cee16066350f03ff04dba
luisccf <luisccf@me.com>
2017-03-18 10:28:56 -0300

    Altera plugin de seleção de horário

--- static/dist/js/script.js
+++ static/dist/js/script.js
@@ -2,11 +2,7 @@ $(document).ready(function(){
 
     $.datetimepicker.setLocale('pt-BR');
 
-    $('[widget=time]').datetimepicker({
-        datepicker: false,
-        mask: true,
-        format: 'H:i'
-    });
+    $('[widget=time]').mask('00:00');
 
     $('[widget=date]').datetimepicker({
         timepicker: false,
