5049c2117d0ac9bdebc13204cbc1dac0eb5fc874
Gabriela Ramalho <gabrielaramalho96@gmail.com>
2017-04-30 00:45:04 -0300

    Comandos SQL para cadastrar os campis do CEFET-MG no banco de dados

---
+++ inserirdados.sql
@@ -0,0 +1,8 @@
+INSERT INTO website_campus VALUES(null,'BH','');
+INSERT INTO website_campus VALUES(null,'Contagem','');
+INSERT INTO website_campus VALUES(null,'Divinopolis','');
+INSERT INTO website_campus VALUES(null,'Leopoldina','');
+INSERT INTO website_campus VALUES(null,'Nepomuceno','');
+INSERT INTO website_campus VALUES(null,'Timóteo','');
+INSERT INTO website_campus VALUES(null,'Varginha','');
+
